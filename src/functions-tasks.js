c8357db810bcec2738b7f546f4aa2d384a284432
valeryaosta <gerasimenko.valery@gmail.com>
2024-04-05 14:37:08 +0300

    feat: add solution for the task retry

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -142,8 +142,18 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return function check() {
+    let lastError;
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        return func();
+      } catch (error) {
+        lastError = error;
+      }
+    }
+    throw lastError;
+  };
 }
 
 /**
