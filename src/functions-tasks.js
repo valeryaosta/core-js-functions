c553c830dd10e700b1d6a5a02d20fa0b9f209cd2
valeryaosta <gerasimenko.valery@gmail.com>
2024-04-05 15:07:18 +0300

    feat: add solution for the task getIdGeneratorFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -226,8 +226,13 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let currentId = startFrom - 1;
+
+  return () => {
+    currentId += 1;
+    return currentId;
+  };
 }
 
 module.exports = {
